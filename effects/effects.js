e17e974dcfba492dc7671e45b1d6a32d795f5b2b
= <axeljimenez045@gmail.com>
2025-04-21 06:48:52 -0400

    new skills

--- effects/effects.js
+++ effects/effects.js
@@ -27,5 +27,6 @@ function MenosImagenes(){
   };
 
 window.addEventListener("scroll", function(){
-  OptionsLinks.classList.toggle("boton2oculto", scrollY>4000);
+  OptionsLinks.classList.toggle("boton2oculto", scrollY>300);
 })
+

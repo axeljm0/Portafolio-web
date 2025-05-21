83195ff6a79e684d4b6b495593701905b53c0a29
= <axeljimenez045@gmail.com>
2025-05-21 01:40:19 -0400

    ecomerce

--- effects/effects.js
+++ effects/effects.js
@@ -1,28 +1,3 @@
-const Nav = document.querySelector(".options");
-const body = document.querySelector("Body")
-const OptionsLinks = document.querySelector(".options");
-const BTNanimacion = document.querySelector("body section footer");
-const BTNsDarkandLight = document.getElementById("BTNdark");
-const Promesa1 = document.querySelector("cool-slider-info");
-const Curriculum = document.getElementById("")
-const btnmas = document.querySelector("")
-
-function OtrasImagenes(){
-  const MasSKILS = document.querySelector(".Mas-Sobre-Skills").style.display = "block";
-  const BTNshowless = document.querySelector(".botonMenosSkilss").style.display = "block";
-  };
-
-function MenosImagenes(){
-  document.querySelector(".Mas-Sobre-Skills").style.display = "none";
-  document.querySelector(".botonMenosSkilss").style.display = "none";
-  };
-
-if(scrollY > 250){
-  OptionsLinks.remove("options");}
-else{
-  OptionsLinks.appendChild("boton2oculto")
-}
-
 const formulario = document.getElementById('form');
 const dialogo = document.getElementById('mensajeDialogo');
 const cerrar = document.getElementById('cerrarDialogo');
@@ -36,3 +11,12 @@ formulario.addEventListener('submit', function(e) {
   });
    }
 )
+
+window.addEventListener("scroll", function () {
+  const elemento = document.getElementById(".options");
+  if (window.scrollY > 150) {
+    elemento.classList.add("oculto");
+  } else {
+    elemento.classList.remove("oculto");
+  }
+});
